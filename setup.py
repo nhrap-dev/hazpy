fd88e4c675cddb4b9a8715c477f327845b9a7bad
ujvalaks <ujvalak_in@yahoo.com>
2020-07-26 21:17:15 -0700

    Version set
    Version set

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
