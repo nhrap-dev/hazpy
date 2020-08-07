0fb397d62c1c72ad147e1c6840d55ea2147f1660
ujvalaks <ujvalak_in@yahoo.com>
2020-08-07 11:06:29 -0700

    version discrepancies fix
    version discrepancies fix

--- hazpy/__init__.py
+++ hazpy/__init__.py
@@ -12,7 +12,7 @@
     
 """
 
-__version__ = '0.0.2.dev1'
+__version__ = '0.0.2.dev5'
 __all__ = ['Tornado', 'Earthquake', 'Hurricane',
            'Tsunami', 'Flood', 'legacy', 'common', 'admin']
 
