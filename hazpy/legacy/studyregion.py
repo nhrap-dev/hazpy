709b5a4be7cfad934d9278309e0414f6d9e8d36c
Raines II <0070642225@FEMA.DHS.GOV>
2020-09-30 14:51:23 -0500

    sql driver StudyRegion

--- hazpy/legacy/studyregion.py
+++ hazpy/legacy/studyregion.py
@@ -93,24 +93,40 @@ class StudyRegion():
                 raise Exception('Method setReturnPeriod failed: Unable to set the returnPeriod.',
                                 'Reinitialize the StudyRegion class and specify the return period as one of the following: ' + str(returnPeriods))
 
-    def createConnection(self):
+    def createConnection(self, orm='pyodbc'):
         """ Creates a connection object to the local Hazus SQL Server database
 
             Key Argument:
-                orm: string -- type of connection to return (choices: 'pyodbc', 'sqlalchemy')
+                orm: string - - type of connection to return (choices: 'pyodbc', 'sqlalchemy')
             Returns:
                 conn: pyodbc connection
         """
         try:
-            comp_name = os.environ['COMPUTERNAME']
-            server = comp_name+"\HAZUSPLUSSRVR"
-            user = 'SA'
-            password = 'Gohazusplus_02'
-            driver = 'ODBC Driver 13 for SQL Server'
-            # driver = 'ODBC Driver 11 for SQL Server'
-            engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(urllib.parse.quote_plus(
-                "DRIVER={0};SERVER={1};PORT=1433;DATABASE={2};UID={3};PWD={4};TDS_Version=8.0;".format(driver, server, self.name, user, password))))
-            conn = engine.connect()
+            # list all Windows SQL Server drivers
+            drivers = [
+                '{ODBC Driver 17 for SQL Server}',
+                '{ODBC Driver 13.1 for SQL Server}',    
+                '{ODBC Driver 13 for SQL Server}',
+                '{ODBC Driver 11 for SQL Server} ',
+                '{SQL Server Native Client 11.0}',
+                '{SQL Server Native Client 10.0}',
+                '{SQL Native Client}',
+                '{SQL Server}'
+            ]
+            computer_name = os.environ['COMPUTERNAME']
+            if orm == 'pyodbc':
+                # create connection with the latest driver
+                for driver in drivers:
+                    try:
+                        conn = py.connect('Driver={d};SERVER={cn}\HAZUSPLUSSRVR; UID=SA;PWD=Gohazusplus_02'.format(
+                            d=driver, cn=computer_name))
+                        break
+                    except:
+                        continue
+            # TODO add sqlalchemy connection
+            # if orm == 'sqlalchemy':
+            #     conn = create_engine('mssql+pyodbc://SA:Gohazusplus_02@HAZUSPLUSSRVR')
+            # self.conn = conn
             return conn
         except:
             print("Unexpected error:", sys.exc_info()[0])
