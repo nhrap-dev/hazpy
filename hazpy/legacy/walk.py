c43e9660bcf7ea609dca1d75f74534f15da34ef2
Raines II <0070642225@FEMA.DHS.GOV>
2020-11-19 16:11:17 -0600

    FLHazardGeoDataFrame

--- hazpy/legacy/walk.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = r'C:\HazusData\Regions\Joachim_9ft_10ft_2'
-
-for root, dirs, files in os.walk(path, topdown=False):
-    for name in files:
-        full_path = os.path.join(root, name)
-        if full_path.endswith('w001001.adf') and ('rpd' in full_path or 'mix0' in full_path ):
-            print(full_path)
